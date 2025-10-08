88302f062d74b39302c7a8657a499467652470c7
Jonel <jonel.richardson@pursuit.org>
2025-10-07 21:56:39 -0400

    Fix: Remove comment breaking imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import eruda from 'eruda';
 eruda.init();
 
-// ... rest of your existing codeimport { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
