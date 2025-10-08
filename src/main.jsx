3e00990c6fac05971c59047c2551f7cd3d8ce97b
Jonel <jonel.richardson@pursuit.org>
2025-10-07 21:53:36 -0400

    Add Eruda console for mobile debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
-import { StrictMode } from 'react'
+import eruda from 'eruda';
+eruda.init();
+
+// ... rest of your existing codeimport { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
